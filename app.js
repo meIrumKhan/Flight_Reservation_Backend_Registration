3fc1b783cf2824098a2f3cb87c86bd83dba876e4
meIrumKhan <meirum880@gmail.com>
2024-12-19 14:41:51 +0500

    update files

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ server.use(express.static("public"));
 
 server.use(
   cors({
-    origin: ["http://localhost:3000", "https://fyp-frontend-roan.vercel.app"],
+    origin: ["http://localhost:3000", "https://fyp-frontend-roan.vercel.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     // allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
